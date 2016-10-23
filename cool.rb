706f885815c1aa11257ce0e146291e3a4c80b42c
The A-Team <alexisabrenica@Andrews-MacBook-Air.local>
2016-10-23 14:49:45 -0400

    cool by location exercise

--- cool.rb
+++ cool.rb
@@ -3,7 +3,21 @@
 #if the location is equal to manhattan, show the user a message "you are cool"
 #in all other circumstances, show a message that says "you are not cool" 
 
+def cool_by_location(location)
+	if location == "brooklyn"
+		puts("you are so cool")
+	elsif location == "manhattan"
+		puts("you are cool")
+	else 
+		puts("you are not cool")
+	end
+end
+
 # ask the user for where they live
+puts("Where do you live?")
+
 # store that information in a variable
+user_location = gets.strip
 
 # call the fuction
+cool_by_location(user_location)
